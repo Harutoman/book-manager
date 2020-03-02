c19d55889953a28ca2060f172bd1ec21dbf34857
ハルトマン <nicoru3315611@gmail.com>
2020-03-02 16:47:11 +0900

    本管理ページの作成

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,39 +1,30 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-    <v-app>
-        <v-content>
-            <v-container fluid fill-height>
-                <v-layout wrap>
-                    <v-btn color='primary'>Button</v-btn>
-                </v-layout>
-            </v-container>
-        </v-content>
-    </v-app>
-                    <div class="card-body">
-                        <ul>
-                            <li v-for="(book,index) in books" v-bind:key="index">{{ book.title }}</li>
-                        </ul>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div>
+  <v-card class="mx-auto" max-width="1500px">
+    <v-layout row wrap>
+      <v-flex v-for="(book,index) in books" :key="index">
+        <v-card class="grey lighten-3 pt-2 m-4" width="200px" height="90%">
+          <v-img :src= "require('../../../image/' + book.id + '.png')" aspect-ratio="1.1" contain/>
+          <v-divider class="mt-2 mb-2" :inset="inset"></v-divider>
+          <div class="pl-2">タイトル</div>
+          <div class="pl-2">{{ book.title }}</div>
+          <v-divider class="mt-2 mb-2" :inset="inset"></v-divider>
+          <div class="pl-2">作者名</div>
+          <div class="pl-2 pb-2">{{ book.author }}</div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        data(){
-            return {
-                books: []
-            }
-        },
-        mounted() {
-          axios.get('/book/data').then(response => this.books= response.data)
-        }
+  export default {
+    data(){
+      return {
+        books: []
+      }
+    },
+    mounted() {
+      axios.get('/book/data').then(response => this.books= response.data)
     }
+  }
 </script>
