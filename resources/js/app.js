c3dab1607cc9191a11e11169208197eb8d55135d
ハルトマン <nicoru3315611@gmail.com>
2020-10-27 15:52:17 +0900

    サイドバーの無効化

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('book-list', require('./components/book-list.vue').default);
-Vue.component('side-bar', require('./components/side-bar.vue').default);
+// Vue.component('side-bar', require('./components/side-bar.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
