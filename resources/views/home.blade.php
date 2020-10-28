4d21c54d5d966afa852633b0cbdfb334b489443a
ハルトマン <nicoru3315611@gmail.com>
2020-10-28 22:16:35 +0900

    テキストの微修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="card text-center" style="margin-left: 80px; margin-right: 80px">
   <b class="mt-3 h3">書籍管理サイトへようこそ！</b>
   <b class="mt-3 h4">このサイトは、ユーザーがお持ちの書籍を管理することを目的としたサイトです。</b>
-  <b class="mb-3 h4 text-danger">※現在、登録機能の方は鋭意開発中です！</b>
+  <b class="mb-3 h4 text-danger">※現在、書籍登録機能の方は鋭意開発中です！</b>
   <b class="mt-3 h4">↓書籍管理画面のサンプル画像です！</b>
   <img class="mt-2 mb-3 mx-auto" style="max-width: 60%;" src="images/sample1.png">
 </div>
